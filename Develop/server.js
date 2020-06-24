c44ffd80ca6c62e09801618f77c0dd3b42e9c110
Heather Empson <hrempson@gmail.com>
2020-06-23 18:17:24 -0700

    fixed root folders

--- Develop/server.js
+++ Develop/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded( {extended: true}));
 
-app.use(express.static("public"));
+app.use(express.static("./Develop/public"));
 
 app.use("/api", apiRoutes);
 app.use(htmlRoutes);
